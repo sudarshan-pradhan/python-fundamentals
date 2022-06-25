86f8e12b0a0b7dce1eff8a7371b8ed1ce5f16c52
Sudarshan Pradhan <81807230+sudarshan-pradhan@users.noreply.github.com>
2022-06-25 12:09:23 +0530

    Initial commit

---
+++ My-App/hello-world.py
@@ -0,0 +1,16 @@
+# 1st python program.
+print("Hello World")
+
+# Learning print statement
+print(50)
+print(1000)
+print(3.142)
+
+# multi argument print statement
+print(50, 1000, 3.142, "Hello World")
+
+print("Hello", end="")
+print("World")
+
+print("Hello", end=" ")
+print("World")
